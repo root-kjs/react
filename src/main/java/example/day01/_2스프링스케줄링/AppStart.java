d4d88e1cd869e867db64f7ca8486276beab30cc6
itdanjalog <kgs2072@naver.com>
2025-09-01 12:18:34 +0900

    day68

---
+++ src/main/java/example/day01/_2스프링스케줄링/AppStart.java
@@ -0,0 +1,14 @@
+package example.day01._2스프링스케줄링;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling // 스케줄링 사용 활성화
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run( AppStart.class );
+    }
+}
