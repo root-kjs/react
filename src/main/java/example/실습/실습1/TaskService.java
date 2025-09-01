10264fb33adff178990027d03ae9cc8344c82068
itdanjalog <kgs2072@naver.com>
2025-09-01 14:32:47 +0900

    day68

--- src/main/java/example/실습/실습1/TaskService.java
+++ src/main/java/example/실습/실습1/TaskService.java
@@ -9,19 +9,19 @@ public class TaskService {
     @Scheduled( cron = " 0/30 * * * * * ") // 예시] 12:57:00 , 12:57:30
     public void task1(){
         System.out.println("TaskService.task1");
-        // ++DAO 호출하여 모든 제품 3개씩 감소 요청/응답++
+        // ++ DAO 호출하여 모든 제품 3개씩 감소 요청 ++
     }
     // 2. 매 1분마다 모든 제품 정보를 조회/출력 한다.
     @Scheduled( cron = " 0 0/1 * * * *") // 예시1] 12:57:00 , 12:58:00
     // @Scheduled( cron = " 0 1 * * * *") // 예시2] 12:01:00 , 13:01:00
     public void task2(){
         System.out.println("TaskService.task2");
-        // ++ 모든 제품 정보를 조회/출력 한다. ++
+        // ++ DAO 호출하여 모든 제품 정보를 조회 요청 하여 출력 ++
     }
     // 3. 매 5분마다 재고가 10 이하인 상품의 재고를 +20개 추가한다.
     @Scheduled( cron = " 0 0/5 * * * *") // 예시1] 12:55:00 , 13:00:00 , 13:05:00
     public void task3(){
         System.out.println("TaskService.task3");
-        // ++ 재고가 10 이하인 상품의 재고를 +20개 추가한다.++
+        // ++ DAO 호출하여 재고가 10 이하인 상품의 재고를 +20개 추가 요청.++
     }
 }
