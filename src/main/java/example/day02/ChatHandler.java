13165af38b285e42028b297090e97750b0e93dc9
itdanjalog <kgs2072@naver.com>
2025-09-02 11:27:27 +0900

    day69

--- src/main/java/example/day02/ChatHandler.java
+++ src/main/java/example/day02/ChatHandler.java
@@ -14,18 +14,21 @@ public class ChatHandler extends TextWebSocketHandler {
     // 1. 클라이언트 소켓이 서버소켓으로부터 연결을 성공했을때 실행되는 메소드
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
+        System.out.println("[서버] 클라이언트소켓과 연동 성공");
+        // WebSocketSession 이란 : ws 기반으로 클라이언트가 요청한 정보가 저장된 객체
+        // HttpSession 이란 : http 기반으로 클라이언트가 요청한 정보가 저장된 객체
+        System.out.println("[클라이언트 웹소켓 객체] : " + session );
     }
 
     // 2. 클라이언트 소켓이 서버소켓 과 연결을 끊겼을때 실행되는 메소드
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-
+        System.out.println("[서버] 클라이언트소켓과 연동 종료 ");
     }
 
     // 3. 클라이언트 소켓이 서버소켓 에게 메시지를 보냈을때 실행되는 메소드
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-
+        System.out.println("[서버] 클라이언트로부터 메시지가 도착");
     }
 }
