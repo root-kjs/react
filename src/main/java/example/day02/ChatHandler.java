9dc3ea3a8aa99b7ac81044b64c427cda8f49c7ee
itdanjalog <kgs2072@naver.com>
2025-09-02 10:28:41 +0900

    day69

---
+++ src/main/java/example/day02/ChatHandler.java
@@ -0,0 +1,31 @@
+package example.day02;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component // 스프링 컨테이너(메모리)에 빈(객체) 등록
+public class ChatHandler extends TextWebSocketHandler {
+
+    // 1. 클라이언트 소켓이 서버소켓으로부터 연결을 성공했을때 실행되는 메소드
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+    }
+
+    // 2. 클라이언트 소켓이 서버소켓 과 연결을 끊겼을때 실행되는 메소드
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+
+    }
+
+    // 3. 클라이언트 소켓이 서버소켓 에게 메시지를 보냈을때 실행되는 메소드
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+    }
+}
