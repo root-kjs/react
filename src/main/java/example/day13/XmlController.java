55973976cb9487f3696fd3ebbcfdd68edad5ccf6
root-kjs <root.kjs82@gmail.com>
2025-10-13 12:25:44 +0900

    Mapper 추상 메소드 int 반환값은 레코드 수이다 <!-- 1. 등록  id="추상메소드명" 매핑을 하겠다. parameterType="매개변수(클래스) 경로 타입"  -->     <insert id="save" parameterType="example.day13.StudentDto" useGeneratedKeys="true" keyProperty="sno" >// @Options( useGeneratedKeys = true, keyProperty = "sno") 하게 되면 Dto sno를 반환한다. // 4. 개별 삭제     int delete( int sno ); // 여기서 int 타입이 가르키는 것은 완료된 레코드의 수를 지칭함.

--- src/main/java/example/day13/XmlController.java
+++ src/main/java/example/day13/XmlController.java
@@ -1,6 +1,8 @@
 package example.day13;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +19,14 @@ public class XmlController {
     public ResponseEntity<?> save(@RequestBody StudentDto studentDto ){
         // < ? > ----> 제네릭 타입에 ? 넣으면 와일드 카드 됨. 모든 타입을 지칭한다.
         // .ok( true ) ----> 소괄호 안에 모든 타입 데이터 입력 가능해진다.
+        // int 로 하게 되면 완료된 레코드의 수
+        // @Options( useGeneratedKeys = true, keyProperty = "sno") 하게 되면 Dto sno를 반환한다.
+        System.out.println("studentDto = " + studentDto); // studentDto = StudentDto(sno=0, name=11, kor=11, math=11)
         xmlMapper.save(studentDto);
+
+        System.out.println("studentDto = " + studentDto); // studentDto = StudentDto(sno=6, name=11, kor=11, math=11)
         return ResponseEntity.ok( true );
+
     }
 
     // 2. 전체조회
@@ -29,5 +37,34 @@ public class XmlController {
         List<StudentDto> result = xmlMapper.findAll();
         return ResponseEntity.ok( result );
     }
+
+    // 3. 개별 조회
+    @GetMapping("/s")
+    public ResponseEntity<?> find( @RequestParam int sno ){
+        // < ? > ----> 제네릭 타입에 ? 넣으면 와일드 카드 됨. 모든 타입을 지칭한다.
+        // .ok( true ) ----> 소괄호 안에 모든 타입 데이터 입력 가능해진다.
+        StudentDto result = xmlMapper.find( sno );
+        return ResponseEntity.ok( result );
+    }
+
+    // 4. 개별 삭제 int sno : 레코드 수를 반환 // Dto dp sno 매핑됨
+    @DeleteMapping
+    public ResponseEntity<?> delete( @RequestParam int sno ){
+        // < ? > ----> 제네릭 타입에 ? 넣으면 와일드 카드 됨. 모든 타입을 지칭한다.
+        // .ok( true ) ----> 소괄호 안에 모든 타입 데이터 입력 가능해진다.
+        xmlMapper.delete( sno );
+        return ResponseEntity.ok( true );
+    }
+
+    // 5. 개별 수정 StudentDto studentDto
+    @PutMapping
+    public ResponseEntity<?> update( @RequestBody StudentDto studentDto ){
+        // < ? > ----> 제네릭 타입에 ? 넣으면 와일드 카드 됨. 모든 타입을 지칭한다. 모든 자료가 대입된다.
+        // .ok( true ) ----> 소괄호 안에 모든 타입 데이터 입력 가능해진다.
+        xmlMapper.update( studentDto );
+        return ResponseEntity.ok( true );
+    }
+
+
 
 }// class end
--- src/main/java/example/day13/XmlMapper.java
+++ src/main/java/example/day13/XmlMapper.java
@@ -1,6 +1,8 @@
 package example.day13;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 import java.util.List;
 
@@ -9,9 +11,20 @@ public interface XmlMapper {
     // 1. 등록
     // 방법1 : 추상 메소드 위에 @Insert("sql") 작성
     // 방법2 : 추상메소드를 매핑하는 방법(XML) 복잡한 SQL 권장
+    // @Insert("insert into student(name, kor, math ) values(#{name}, #{kor}, #{math} )")
+    // @Options( useGeneratedKeys = true, keyProperty = "sno") // 생성된 pk을 sno 필드에 매핑
     int save( StudentDto studentDto );
 
     // 2. 전체 조회
     List<StudentDto> findAll();
+    
+    // 3. 개별 조회
+    StudentDto find(int sno );
+
+    // 4. 개별 삭제
+    int delete( int sno ); // 여기서 int 타입이 가르키는 것은 완료된 레코드의 수를 지칭함.
+
+    // 5. 개별 수정
+    int update( StudentDto studentDto );
 
 }// i end
